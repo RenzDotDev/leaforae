c534c92dc7b8f18e0297c6ec745109e9d4f3474e
Rdev-Prog <tolentinorenzo23dev@gmail.com>
2025-09-04 21:47:01 +0800

    New Changes Saved

--- src/assets/Landing_Page/component/heroSection.jsx
+++ src/assets/Landing_Page/component/heroSection.jsx
@@ -12,25 +12,34 @@ function HeroSection() {
           <div className="text-con">
             <p>Bring Home the <span style={{color: "#3A5A40"}}>Green</span></p>
             <p>Fresh plants, happy spaces, and a touch of nature for every home.</p>
+        
+            {/* Button Container */}
+            <div className="button-con">
+              <Link to={"/Product"} className="btn shop-now-btn">
+                <p>Shop now</p>
+                <i className="fa-solid fa-arrow-right"></i>
+              </Link>
+
+              <Link to={"/Blog"} className="btn explore-now-btn">
+                <p>Read blogs</p>
+                <i className="fa-solid fa-arrow-right"></i>
+              </Link>
+            </div>
           </div>
 
-          {/* Button Container */}
-          <div className="button-con">
-            <button className="shop-now-btn">
-              <p>Shop now</p>
-              <i className="fa-solid fa-arrow-right"></i>
-            </button>
-
-            <button className="explore-now-btn">
-              <p>Explore now</p>
-              <i className="fa-solid fa-arrow-right"></i>
-            </button>
-          </div>
 
           {/* Action Container */}
           <div className="action-con">
-            <Link to={"/Product"}>Product</Link>
-            <Link to={"/Product"}>Product</Link>
+            <Link to={"/Product"} className='plant-health-btn'>
+              <img src="plant-image6.png" alt="" />
+              <p className='plant-healt-txt'>Plants for Health</p>
+              <i className="fa-solid fa-arrow-right"></i>
+            </Link>
+            <Link to={"/Product"} className='plant-interior-btn'>
+              <img src="plant-image5.png" alt="" />
+              <p className='plant-healt-txt'>Plants for Interior</p>
+              <i className="fa-solid fa-arrow-right"></i>
+            </Link>
           </div>
         </section>
 
