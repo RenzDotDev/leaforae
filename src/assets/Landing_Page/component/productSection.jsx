2a15248d20d4fe06c891459dcbdb27597f3aadb5
Rdev-Prog <tolentinorenzo23dev@gmail.com>
2025-09-17 07:11:47 +0800

    Put images

--- src/assets/Landing_Page/component/productSection.jsx
+++ src/assets/Landing_Page/component/productSection.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+
 import "../style/productSection.css"
 
 function ProductSection() {
+
   const succulentPlants = [
     { name: "Jade plant", url: "https://perenual.com/storage/species_image/2193_crassula_ovata/medium/33253726791_980c738a1e_b.jpg"},
     { name: "Aloe vera", url: "https://perenual.com/storage/species_image/728_aloe_vera/medium/52619084582_6ebcfe6a74_b.jpg"},
@@ -11,26 +13,14 @@ function ProductSection() {
 
   const fernPlants = [
     { name: "Maidenhair fern", url: "https://perenual.com/storage/species_image/543_adiantum_capillus-veneris/medium/49636882698_cb4fa4c685_b.jpg"},
-    { name: "Tulip", url: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Tulip_-_floriade_canberra.jpg"},
-    { name: "Sunflower", url: "https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg"}
+    { name: "Japanese painted fern", url: "https://perenual.com/storage/species_image/1114_athyrium_branford_rambler/medium/1536px-Athyrium_niponicum_27Red_Beauty27_kz02.jpg"},
+    { name: "Maidenhair spleenwort", url: "https://perenual.com/storage/species_image/1043_asplenium_trichomanes/medium/51698793799_a69d99ba08_b.jpg"}
   ]
 
   const flowerPlants = [
-    { name: "Rose", url: "https://upload.wikimedia.org/wikipedia/commons/b/bf/Red_rose.jpg"},
-    { name: "Tulip", url: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Tulip_-_floriade_canberra.jpg"},
-    { name: "Sunflower", url: "https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg"}
-  ]
-
-  const treePlants = [
-    { name: "Rose", url: "https://upload.wikimedia.org/wikipedia/commons/b/bf/Red_rose.jpg"},
-    { name: "Tulip", url: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Tulip_-_floriade_canberra.jpg"},
-    { name: "Sunflower", url: "https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg"}
-  ]
-
-  const herbPlants = [
-    { name: "Rose", url: "https://upload.wikimedia.org/wikipedia/commons/b/bf/Red_rose.jpg"},
-    { name: "Tulip", url: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Tulip_-_floriade_canberra.jpg"},
-    { name: "Sunflower", url: "https://upload.wikimedia.org/wikipedia/commons/4/40/Sunflower_sky_backdrop.jpg"}
+    { name: "Giant milkweed", url: "https://perenual.com/storage/species_image/1509_calotropis_gigantea/medium/24847034070_f858b07333_b.jpg"},
+    { name: "Tickseed", url: "https://perenual.com/storage/species_image/2047_coreopsis_auriculata_nana/medium/1971324341_d79a58e3a9_b.jpg"},
+    { name: "Horn of Plenty", url: "https://perenual.com/storage/species_image/2319_datura_metel/medium/48861017193_05f55aca0f_b.jpg"}
   ]
 
   const [activeCategory, setActiveCategory] = useState("succulents")
@@ -42,8 +32,6 @@ function ProductSection() {
     if (category === "succulents") setPlants(succulentPlants)
     if (category === "ferns") setPlants(fernPlants)
     if (category === "flowers") setPlants(flowerPlants)
-    if (category === "trees") setPlants(treePlants)
-    if (category === "herbs") setPlants(herbPlants)
     // add ferns / trees / herbs here later
   }
 
@@ -83,8 +71,6 @@ function CategoryList({ activeCategory, onCategoryChange }) {
       <button onClick={() => onCategoryChange("succulents")} className={activeCategory === "succulents" ? "active-category" : ""}>Succulents</button>
       <button onClick={() => onCategoryChange("ferns")} className={activeCategory === "ferns" ? "active-category" : ""}>Ferns</button>
       <button onClick={() => onCategoryChange("flowers")} className={activeCategory === "flowers" ? "active-category" : ""}>Flowers</button>
-      <button onClick={() => onCategoryChange("trees")} className={activeCategory === "trees" ? "active-category" : ""}>Trees</button>
-      <button onClick={() => onCategoryChange("herbs")} className={activeCategory === "herbs" ? "active-category" : ""}>Herbs</button>
     </section>
   )
 }
