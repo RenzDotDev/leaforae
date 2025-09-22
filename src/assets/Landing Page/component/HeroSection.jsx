fb126d9712c3b2ebd1a6cd7c09dd22b290eb6a0b
Rdev-Prog <tolentinorenzo23dev@gmail.com>
2025-09-22 14:18:30 +0800

    Create animations

--- src/assets/Landing Page/component/HeroSection.jsx
+++ src/assets/Landing Page/component/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "motion/react";
 import Header from "../../Header/Header";
 
 function HeroSection() {
@@ -6,23 +7,50 @@ function HeroSection() {
     <div className="grid w-full h-full grid-rows-auto">
       <Header />
       <section className="p-3 flex flex-col items-center justify-between text-center h-[90dvh]">
-        <img src="/plant-image2.png" alt="plant image" className="h-[60%]" />
-        <p className="text-5xl font-semibold">
+        <motion.img
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 0.2 }}
+          src="/plant-image2.png"
+          alt="plant image"
+          className="h-[60%]"
+        />
+        <motion.p
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.2 }}
+          className="text-5xl font-semibold"
+        >
           Bring Home the <span className="text-darkGreen">Green</span>
-        </p>
-        <p className="text-sm">
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.3 }}
+          className="text-sm"
+        >
           Fresh plants, happy spaces, and a touch of nature for every home.
-        </p>
+        </motion.p>
 
         <div className="flex gap-2 items-center just-center">
-          <button className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg bg-darkGreen">
+          <motion.button
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.2 }}
+            className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg bg-darkGreen"
+          >
             <p className="text-white text-sm">Shop Now</p>
             <i className="fa-solid fa-arrow-right text-white"></i>
-          </button>
-          <button className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg">
+          </motion.button>
+          <motion.button
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3 }}
+            className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg"
+          >
             <p className="text-sm">Read Blogs</p>
             <i className="fa-solid fa-arrow-right"></i>
-          </button>{" "}
+          </motion.button>{" "}
         </div>
       </section>
     </div>
