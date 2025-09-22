b6a141de4cdcba35c1b74777e933058fb7d5d00d
Rdev-Prog <tolentinorenzo23dev@gmail.com>
2025-09-22 14:09:59 +0800

    New Hero Section

--- src/assets/Landing Page/component/HeroSection.jsx
+++ src/assets/Landing Page/component/HeroSection.jsx
@@ -3,24 +3,24 @@ import Header from "../../Header/Header";
 
 function HeroSection() {
   return (
-    <div className="grid w-full h-full grid-rows-[10%_90%]">
+    <div className="grid w-full h-full grid-rows-auto">
       <Header />
-      <section className="p-3 flex flex-col items-center text-center gap-3">
-        <img src="/plant-image2.png" alt="plant image" className="w-[35dvh]" />
+      <section className="p-3 flex flex-col items-center justify-between text-center h-[90dvh]">
+        <img src="/plant-image2.png" alt="plant image" className="w-70" />
         <p className="text-5xl font-semibold">
           Bring Home the <span className="text-darkGreen">Green</span>
         </p>
-        <p className="text-base">
+        <p className="text-sm mt-[-36px]">
           Fresh plants, happy spaces, and a touch of nature for every home.
         </p>
 
         <div className="flex gap-2 items-center just-center">
           <button className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg bg-darkGreen">
-            <p className="text-white">Shop Now</p>
+            <p className="text-white text-sm">Shop Now</p>
             <i className="fa-solid fa-arrow-right text-white"></i>
           </button>
           <button className="flex items-center justify-center gap-2 px-6 py-3 rounded-lg">
-            <p>Read Blogs</p>
+            <p className="text-sm">Read Blogs</p>
             <i className="fa-solid fa-arrow-right"></i>
           </button>{" "}
         </div>
