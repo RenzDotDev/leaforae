9116d58e4bd7c8236c6f7f0752c581357fd1b4ba
Rdev-Prog <tolentinorenzo23dev@gmail.com>
2025-10-14 17:47:05 +0800

    Made some changes

--- src/assets/Cart Page/CartPage.jsx
+++ src/assets/Cart Page/CartPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function CartPage() {
-  return <div>CartPage</div>;
+  return (
+    <div className="grid grid-cols-1 grid-rows-[7%_auto] w-full h-full gap-1.5">
+      <CartHeader />
+      <section className="w-full grid grid-cols-1 h-fit max-h-full gap-2 py-3"></section>
+    </div>
+  );
 }
 
 export default CartPage;
